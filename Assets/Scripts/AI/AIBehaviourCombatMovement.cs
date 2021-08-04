0b1ca5813143128419bf76f35f3bfa2510de8b77
Leonardo Tagliaro <leonardotgo@gmail.com>
2021-08-04 13:39:36 -0300

    Text formatting in AIBehavior class

--- Assets/Scripts/AI/AIBehaviourCombatMovement.cs
+++ Assets/Scripts/AI/AIBehaviourCombatMovement.cs
@@ -5,12 +5,9 @@ using Patterns.Observer;
 
 namespace BrightSouls.AI
 {
-
     public class AIBehaviourCombatMovement : AIBehaviour
     {
-
         // Public Fields
-
         public float minTargetDistance = 1.5f;
         public float maxTargetDistance = 5f;
         public float idealTargetDistance = 4f;
@@ -18,25 +15,19 @@ namespace BrightSouls.AI
 
         private float turnSpeed = 4f;
 
+        // Inspector-assigned values
         [SerializeField] private float minimalAttackCooldown = 0.5f;
         [SerializeField] private float defaultAttackCooldown = 3f;
         public float defenseDelay = 1.5f;
         public float dodgeChance = 0.35f;
 
         private float dashAttackThreshold = 4f;
-
-        private int lightAttackChance = 70;
-
-        // Private Fields
-
-        private bool isAdjustingDistance = false;
-
-        // Public Methods
+        private int   lightAttackChance = 70;
+        private bool  isAdjustingDistance = false;
 
         public override void BehaviourStart()
         {
             isAdjustingDistance = false;
-
             owner.SetMovementControl(AICharacter.AIMovementControlType.Animator);
             owner.Movement = Vector2.zero;
             owner.CurrentMoveSpeed = owner.WalkMoveSpeed;
@@ -52,8 +43,6 @@ namespace BrightSouls.AI
         public override void BehaviourEnd()
         {
         }
-
-        // Private Methods
 
         private void LookAtTarget()
         {
@@ -93,9 +82,11 @@ namespace BrightSouls.AI
 
         private void ChooseAttack()
         {
-            bool canAttack =
-                (fsm.CurrentStateTime > defaultAttackCooldown) ||
-                (owner.Target.IsInAnyState(States.Attacking, States.Comboing) && owner.GetDistanceToTarget() < maxTargetDistance && fsm.CurrentStateTime > minimalAttackCooldown);
+            bool attackOnCooldown = fsm.CurrentStateTime < defaultAttackCooldown;
+            bool comboOnCooldown = fsm.CurrentStateTime < minimalAttackCooldown;
+            bool isInRange = owner.GetDistanceToTarget() < maxTargetDistance;
+            bool isOnCombo = owner.Target.IsInAnyState(States.Attacking, States.Comboing);
+            bool canAttack = !attackOnCooldown || (isOnCombo && isInRange && !comboOnCooldown);
             if (canAttack)
             {
                 if (owner.Target.IsInAnyState(States.Blocking))
@@ -106,17 +97,22 @@ namespace BrightSouls.AI
                 else
                 {
                     if (owner.GetDistanceToTarget() > idealTargetDistance + dashAttackThreshold)
+                    {
                         owner.nextAttack = 2;
+                    }
                     else
                     {
                         int rand = Random.Range(0, 100);
                         if (rand < lightAttackChance)
+                        {
                             owner.nextAttack = 0;
+                        }
                         else
+                        {
                             owner.nextAttack = 1;
+                        }
                     }
                 }
-
                 owner.Notify(Message.AI_StartAttack);
             }
         }
